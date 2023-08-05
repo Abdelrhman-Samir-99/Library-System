81b6d08269ecedbebc38fd14f50668d42893121b
abdulrhman.samir123@gmail.com <abdelrahman.samir@cegedim.com>
2023-08-05 22:30:34 +0300

    Adding the backend using SpringBoot v3.0.9

---
+++ LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/LibrarySystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.selfStudy.LibrarySystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemBackendApplication.class, args);
+	}
+
+}
