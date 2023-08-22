f53f3c3f4a19f1638b7cac340d4c91a1a7d52c5b
abdulrhman.samir123@gmail.com <abdelrahman.samir@cegedim.com>
2023-08-22 23:58:29 +0300

    Adding mappers implementation

--- LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/mappers/IdentificationMapper.java
+++ LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/mappers/IdentificationMapper.java
@@ -2,6 +2,11 @@ package com.selfStudy.LibrarySystemBackend.mappers;
 
 import org.mapstruct.Mapper;
 
+import com.selfStudy.LibrarySystemBackend.dtos.IdentificationDTO;
+import com.selfStudy.LibrarySystemBackend.models.Identification;
+
 @Mapper
 public interface IdentificationMapper {
+	Identification mapDtoToIdentification(IdentificationDTO identificationDTO);
+	IdentificationDTO mapIdentificationToDto(Identification identification);
 }
--- LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/mappers/StudentMapper.java
+++ LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/mappers/StudentMapper.java
@@ -1,7 +1,16 @@
 package com.selfStudy.LibrarySystemBackend.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.selfStudy.LibrarySystemBackend.dtos.StudentDTO;
+import com.selfStudy.LibrarySystemBackend.models.Student;
 
 @Mapper
 public interface StudentMapper {
+	@Mapping(source = "identificationId", target = "identification.id")
+	Student mapStudentDtoToStudent(StudentDTO studentDTO);
+
+	@Mapping(source = "identification.id", target = "identificationId")
+	StudentDTO mapStudentToStudentDto(Student student);
 }
