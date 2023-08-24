c6aacb5aee0fd0e9d54d4f824e5bf326c14de211
abdulrhman.samir123@gmail.com <abdelrahman.samir@cegedim.com>
2023-08-24 22:22:30 +0300

    Adding Student DTO

---
+++ LibrarySystem-Backend/src/main/java/com/selfStudy/LibrarySystemBackend/dtos/StudentDTO.java
@@ -0,0 +1,24 @@
+package com.selfStudy.LibrarySystemBackend.dtos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class StudentDTO {
+	private UUID id;
+	private String firstName;
+	private String lastName;
+	private LocalDate joinDate;
+	private LocalDate graduationDate;
+	private UUID identificationId;
+}
