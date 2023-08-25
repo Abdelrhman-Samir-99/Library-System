d4f4b6571291c39f1e88563a8e3bd2b04c63eb33
abdulrhman.samir123@gmail.com <abdelrahman.samir@cegedim.com>
2023-08-25 23:35:52 +0300

    Changing yaml to yml :)

--- LibrarySystem-Backend/src/test/java/com/selfStudy/LibrarySystemBackend/LibrarySystemBackendApplicationTests.java
+++ LibrarySystem-Backend/src/test/java/com/selfStudy/LibrarySystemBackend/LibrarySystemBackendApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.yaml")
+@TestPropertySource(locations = "classpath:application-test.yml")
 class LibrarySystemBackendApplicationTests {
 
 	@Test
